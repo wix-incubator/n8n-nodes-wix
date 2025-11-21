999f481302b702d1668a1d68d52747ea9db2e792
Itai Ben David <itaibd@wix.com>
2025-11-21 14:34:54 -0500

    ignore no-unresolved lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,17 @@
 import { config } from '@n8n/node-cli/eslint';
 
-export default config;
+export default [
+	...config.slice(0, -1),
+	{
+		...config[config.length - 1],
+		rules: {
+			...(config[config.length - 1]?.rules || {}),
+			'import-x/no-unresolved': [
+				'error',
+				{
+					ignore: ['^n8n-workflow'],
+				},
+			],
+		},
+	},
+];
