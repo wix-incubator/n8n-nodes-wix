8dc2d1ea2850ffc188e396610f971fe27fa859ed
Itai Ben David <itaibd@wix.com>
2025-11-21 14:36:55 -0500

    try to use npm in CI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,3 @@
 import { config } from '@n8n/node-cli/eslint';
 
-export default [
-	...config.slice(0, -1),
-	{
-		...config[config.length - 1],
-		rules: {
-			...(config[config.length - 1]?.rules || {}),
-			'import-x/no-unresolved': [
-				'error',
-				{
-					ignore: ['^n8n-workflow'],
-				},
-			],
-		},
-	},
-];
+export default config;
