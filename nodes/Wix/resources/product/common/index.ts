0095170f98317786f85b3c67596c96334eb5bcbc
Itai Ben David <itaibd@wix.com>
2026-01-07 14:21:21 -0500

    product can update options and modifiers

--- nodes/Wix/resources/product/common/index.ts
+++ nodes/Wix/resources/product/common/index.ts
@@ -2,6 +2,7 @@ export * from './choices';
 export * from './fields';
 export * from './inventory';
 export * from './media';
+export * from './optionsModifiers';
 export * from './physicalProperties';
 export * from './requestedFields';
 export * from './variant';
